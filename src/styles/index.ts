b0e4deadfb4b861141b393921f4d66a7797bd5e3
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-18 15:27:40 -0300

    criando os tamanhos das fontes no tema

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -12,6 +12,13 @@ export const { config, styled, globalCss, keyframes, getCssText, createTheme, th
 
       green500: '#00875f',
       green300: '#00b37e',
-    }
-  }
-})
+    },
+
+    fontSizes: {
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
+    },
+  },
+});
