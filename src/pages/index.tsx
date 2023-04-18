9d9a6c03f7bb8442fb93d18bbc00412bb6d105d6
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-18 15:28:18 -0300

    criando a page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,33 @@
-import { styled } from "../styles"
-
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-})
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+import shirtOne from '../assets/shirts/1.png';
+import shirtTwo from '../assets/shirts/2.png';
+import shirtThree from '../assets/shirts/3.png';
 
 export default function Home() {
   return (
-   <h1>Hello World</h1>
-  )
+   <HomeContainer>
+    <Product>
+      <Image src={shirtOne} width={520} height={480} alt='' />
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,99</span>
+      </footer>
+    </Product>
+    <Product>
+      <Image src={shirtTwo} width={520} height={480} alt='' />
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,99</span>
+      </footer>
+    </Product>
+    <Product>
+      <Image src={shirtThree} width={520} height={480} alt='' />
+      <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,99</span>
+      </footer>
+    </Product>
+   </HomeContainer>
+  );
 }
