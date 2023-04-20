dcbaf07d55efa2c1ea5e458c50fcf132ee8aec90
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-20 16:11:31 -0300

    adicionando um prefetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home({ products }: HomeProps) {
    <HomeContainer ref={sliderRef} className="keen-slider">
     { products.map((product) => {
       return (
-        <Link key={product.id} href={`/product/${product.id}`}>
+        <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
           <Product className="keen-slider__slide">
             <Image src={product.imageUrl} width={520} height={480} alt='' />
             <footer>
