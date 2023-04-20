2595af1693cc30441448289b28563a1aefe73436
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-20 14:55:54 -0300

    criando a estilização da página Product

--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -13,7 +13,7 @@ export const ProductContainer = styled('main', {
 export const ImageContainer = styled('div', {
   width: '100%',
   maxWidth: 576,
-  height: 'calc()',
+  height: 'calc(656px - 0.5rem)',
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100% )',
   borderRadius: 8,
   padding: '0.25rem',
@@ -27,4 +27,42 @@ export const ImageContainer = styled('div', {
   },
 });
 
-export const ProductDetails = styled('div', {})
+export const ProductDetails = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+
+  h1: {
+    fontSize: '$2xl',
+    color: '$gray300',
+  },
+
+  span: {
+    marginTop: '1rem',
+    display: 'block',
+    fontSize: '$2xl',
+    color: '$green300',
+  },
+
+  p: {
+    marginTop: '2.5rem',
+    fontSize: '$md',
+    lineHeight: 1.6,
+    color: '$gray300',
+  },
+
+  button: {
+    marginTop: 'auto',
+    backgroundColor: '$green500',
+    border: 0,
+    color: '$white',
+    borderRadius: 8,
+    padding: '1.25rem',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    fontSize: '$md',
+
+    '&:hover': {
+      backgroundColor: '$green300',
+    },
+  },
+});
