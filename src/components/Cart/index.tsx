a78090b5f5901b6191760e32fd3463cb1503dfb6
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-21 16:18:58 -0300

    chamando a função de remover os produtos do carrinho

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -4,8 +4,12 @@ import { CartClose, CartContent, CartProduct, CartProductImage, CartProductDetai
 import { X } from "phosphor-react";
 import Image from "next/image";
 import shirtImage from '../../assets/4.png';
+import { useCart } from "@/src/hooks/useCart";
 
 export function Cart() {
+  const { cartItems, removeCartItem } = useCart();
+  const cartQuantity = cartItems.length;
+  
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
@@ -18,28 +22,32 @@ export function Cart() {
           </CartClose>
           <h2>Sacola de compras</h2>
           <section>
-           { /* <p>Parece que seu carrinho está vazio</p> */ } 
-           <CartProduct>
-              <CartProductImage>
-                <Image 
-                  width={100} 
-                  height={93} 
-                  alt='shirt'
-                  src={shirtImage}
-                />
-              </CartProductImage>
-              <CartProductDetails>
-                <p>Produto 1</p>
-                <strong>R$ 50,00</strong>
-                <button>Remover</button>
-              </CartProductDetails>
-           </CartProduct>
+            { cartQuantity <= 0 && <p>Parece que seu carrinho está vazio</p> }
+            { cartItems.map((cartItem) => {
+              return (
+                <CartProduct key={cartItem.id}>
+                    <CartProductImage>
+                      <Image 
+                        width={100} 
+                        height={93} 
+                        alt='shirt'
+                        src={cartItem.imageUrl}
+                      />
+                    </CartProductImage>
+                    <CartProductDetails>
+                      <p>{cartItem.name}</p>
+                      <strong>{cartItem.price}</strong>
+                      <button onClick={() => removeCartItem(cartItem.id)}>Remover</button>
+                    </CartProductDetails>
+                </CartProduct>
+              )
+            }) }
           </section>
           <CartFinalization>
             <FinalizationDetails>
               <div>
                 <span>Quantidade</span>
-                <p>2 itens</p>
+                <p>{cartQuantity} { cartQuantity > 1 ? 'itens' : 'item' }</p>
               </div>
               <div>
                 <span>Valor Total</span>
