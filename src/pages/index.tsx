9609ff09b0297b1a2e4c6f03cf5b85a7ff304a96
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-21 15:47:19 -0300

    recriando o slider da página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import { HomeContainer, Product } from "../styles/pages/home";
-import { useKeenSlider } from 'keen-slider/react';
-import 'keen-slider/keen-slider.min.css';
+import { HomeContainer, Product, SliderContainer } from "../styles/pages/home";
 import { stripe } from "../lib/stripe";
 import { GetStaticProps } from "next";
 import Stripe from "stripe";
 import Link from 'next/link'
 import Head from 'next/head';
+import useEmblaCarousel from "embla-carousel-react";
 
 interface HomeProps {
   products: {
@@ -18,33 +17,38 @@ interface HomeProps {
 }
 
 export default function Home({ products }: HomeProps) {
-  const [ sliderRef ] = useKeenSlider({
-    slides: {
-      perView: 3,
-      spacing: 48,
-    },
+  const [ emblaRef ] = useEmblaCarousel({
+    align: 'start',
+    skipSnaps: false,
+    dragFree: true,
   });
-
+  
   return (
     <>
       <Head>
         <title>Home | Ignite Shop</title>
       </Head>
-      <HomeContainer ref={sliderRef} className="keen-slider">
-        { products.map((product) => {
-          return (
-            <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
-              <Product className="keen-slider__slide">
-                <Image src={product.imageUrl} width={520} height={480} alt='' />
-                <footer>
-                  <strong>{product.name}</strong>
-                  <span>{product.price}</span>
-                </footer>
-              </Product>
-            </Link>
-          )
-        }) }
-      </HomeContainer>
+      <div style={{ overflow: 'hidden', width: '100%' }}>
+        <HomeContainer>
+          <div className="embla" ref={emblaRef}>
+            <SliderContainer className="embla__container container">
+              { products.map((product) => {
+                return (
+                  <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
+                    <Product className="embla__slide">
+                      <Image src={product.imageUrl} width={520} height={480} alt='' />
+                      <footer>
+                        <strong>{product.name}</strong>
+                        <span>{product.price}</span>
+                      </footer>
+                    </Product>
+                  </Link>
+                )
+              }) }
+            </SliderContainer>
+          </div>
+        </HomeContainer>
+      </div>
    </>
   );
 }
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -8,6 +8,16 @@ export const HomeContainer = styled('main', {
   minHeight: 656,
 });
 
+export const SliderContainer = styled('div', {
+  display: 'flex',
+  gap: '3rem',
+  margin: '0 auto',
+  
+  '.embla__slide': {
+    minWidth: '43.5rem',
+  },
+});
+
 export const Product = styled('div', {
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100% )',
   borderRadius: 8,
@@ -15,6 +25,8 @@ export const Product = styled('div', {
   position: 'relative',
   overflow: 'hidden',
   minWidth: 540,
+  width: '100%',
+  minHeight: 656,
 
   display: 'flex',
   alignItems: 'center',
