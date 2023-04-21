b26234f10c95107a92818d8f29fcdc952739b1d6
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-21 16:41:54 -0300

    redirecionamento para a página de pagamento

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -5,6 +5,8 @@ import { X } from "phosphor-react";
 import Image from "next/image";
 import shirtImage from '../../assets/4.png';
 import { useCart } from "@/src/hooks/useCart";
+import { useState } from "react";
+import axios from "axios";
 
 export function Cart() {
   const { cartItems, removeCartItem, cartTotal } = useCart();
@@ -13,6 +15,24 @@ export function Cart() {
     style: 'currency',
     currency: 'BRL',
   }).format(cartTotal);
+  const [ isCreatingCheckoutSession, setIsCreatingCheckoutSession ] = useState(false);
+
+  async function handleCheckout() {
+    try {
+      setIsCreatingCheckoutSession(true);
+
+      const response = await axios.post('/api/checkout', {
+        products: cartItems,
+      });
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href =  checkoutUrl;
+    } catch(error) {
+      setIsCreatingCheckoutSession(false);
+      alert('Falha ao redirecionar ao checkout');
+    }
+  }
   
   return (
     <Dialog.Root>
@@ -58,7 +78,12 @@ export function Cart() {
                 <p>{formattedCartTotal}</p>
               </div>
             </FinalizationDetails>
-            <button>Finalizar compra</button>
+            <button 
+              onClick={handleCheckout} 
+              disabled={isCreatingCheckoutSession || cartQuantity <= 0}
+            >
+              Finalizar compra
+            </button>
           </CartFinalization>
         </CartContent>
       </Dialog.Portal>
