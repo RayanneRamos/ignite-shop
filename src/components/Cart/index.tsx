5ccadc141dc83173ac3091e172a92f19400ab753
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-21 15:36:54 -0300

    criando os componentes do Cart e a sua estilização

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,7 +1,9 @@
 import { CartButton } from "../CartButton";
 import * as Dialog from '@radix-ui/react-dialog';
-import { CartClose, CartContent } from "./styles";
+import { CartClose, CartContent, CartProduct, CartProductImage, CartProductDetails, CartFinalization, FinalizationDetails } from "./styles";
 import { X } from "phosphor-react";
+import Image from "next/image";
+import shirtImage from '../../assets/4.png';
 
 export function Cart() {
   return (
@@ -16,8 +18,36 @@ export function Cart() {
           </CartClose>
           <h2>Sacola de compras</h2>
           <section>
-            <p>Parece que seu carrinho está vazio</p>
+           { /* <p>Parece que seu carrinho está vazio</p> */ } 
+           <CartProduct>
+              <CartProductImage>
+                <Image 
+                  width={100} 
+                  height={93} 
+                  alt='shirt'
+                  src={shirtImage}
+                />
+              </CartProductImage>
+              <CartProductDetails>
+                <p>Produto 1</p>
+                <strong>R$ 50,00</strong>
+                <button>Remover</button>
+              </CartProductDetails>
+           </CartProduct>
           </section>
+          <CartFinalization>
+            <FinalizationDetails>
+              <div>
+                <span>Quantidade</span>
+                <p>2 itens</p>
+              </div>
+              <div>
+                <span>Valor Total</span>
+                <p>R$ 100,00</p>
+              </div>
+            </FinalizationDetails>
+            <button>Finalizar compra</button>
+          </CartFinalization>
         </CartContent>
       </Dialog.Portal>
     </Dialog.Root>
