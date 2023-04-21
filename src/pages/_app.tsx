886f32041010c22248dd36f542d02ff7bc294e7f
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-21 14:42:42 -0300

    removendo o Header do arquivo App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
 import { globalStyles } from '../styles/global'
-import Image from 'next/image';
-import logoImage from '../assets/logo.svg'
-import { Container, Header } from '../styles/pages/app';
+import { Container } from '../styles/pages/app';
+import Header from '../components/Header';
 
 const roboto = Roboto({
   weight: ['400', '700'],
@@ -16,9 +15,7 @@ globalStyles();
 export default function App({ Component, pageProps }: AppProps) {  
   return (
     <Container className={roboto.className}>
-      <Header>
-        <Image src={logoImage} alt='Logo' />
-      </Header>
+      <Header />
       <Component {...pageProps} />
     </Container>
   )
