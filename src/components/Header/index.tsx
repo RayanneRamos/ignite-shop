156401f0c99dc9ec3e510894a17053943280d6e5
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-21 17:10:02 -0300

    validação para esconder o botão

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,19 @@ import logoImage from '../../assets/logo.svg'
 import { HeaderContainer } from "./styles";
 import Link from "next/link";
 import { Cart } from "../Cart";
+import { useRouter } from "next/router";
 
 export function Header() {
+  const { pathname } = useRouter();
+
+  const showCartButton = pathname !== '/success';
+  
   return (
     <HeaderContainer>
       <Link href='/'>
         <Image src={logoImage} alt='Logo' />
       </Link>
-      <Cart />
+      { showCartButton && <Cart /> }
     </HeaderContainer>
   );
 }
